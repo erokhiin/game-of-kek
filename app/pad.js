c6e7e7b191b6de45e8f6287fc503d7e05a064c20
Dmitry Podlesny <dpodlesny@nwork.local>
2016-04-24 00:17:15 +0300

    pad development

--- app/pad.js
+++ app/pad.js
@@ -1,51 +1,112 @@
 import './pad.css';
 
-// blocks
+window.addEventListener('load', function() {
 
-var nippleWrap = document.querySelector('.pad__nipple-wrap');
-var nipple = document.querySelector('.pad__nipple');
-var btn = document.querySelector('.pad__btn');
-var nippleSide;
-var r;
-var touched;
+  // blocks
 
+  const nippleWrap = document.querySelector('.pad__nipple-wrap');
+  const nipple = document.querySelector('.pad__nipple');
+  const btn = document.querySelector('.pad__btn');
+  let nippleSide;
+  let nippleTouch;
+  let btnTouch;
+  let nippleR;
 
-// nipple
+  // nipple
 
-function posToCoords({x: x, y: y}) {
+  function posToCoords({x: x, y: y}) {
 
-}
-
-function coordsToPos({x: x, y: y}) {
-  return {
-    x: r + (r * x),
-    y: r + (r * y)
-  };
-}
-
-function pos({x: x, y: y}) {
-  nipple.style.top = x + 'px';
-  nipple.style.left = y + 'px';
-}
-
-
-//
-
-
-// resize
-
-function resize() {
-  nippleWrap.style.width = nippleWrap.getBoundingClientRect().height + 'px';
-  nippleWrap.style.left = nippleWrap.getBoundingClientRect().top + 'px';
-  btn.style.width = btn.getBoundingClientRect().height + 'px';
-  btn.style.right = btn.getBoundingClientRect().top + 'px';
-  nippleSide = nipple.getBoundingClientRect().width;
-  r = nippleWrap.getBoundingClientRect().width / 2;
-  nipple.style.marginLeft = nipple.style.marginTop = (- nippleSide / 2) + 'px';
-  if (!touched) {
-    pos(coordsToPos({x: 0, y: 0}));
   }
-};
 
-resize();
-window.onresize = resize;
+  function coordsToPos({x: x, y: y}) {
+    return {
+      x: nippleR + (nippleR * x),
+      y: nippleR + (nippleR * y)
+    };
+  }
+
+  function posNipple({x: x, y: y}) {
+    nipple.style.top = y + 'px';
+    nipple.style.left = x + 'px';
+  }
+
+  function checkIntersection(el, x, y, rm = 0) {
+    const _x = x - el.offsetTop;
+    const _y = y - el.offsetLeft;
+    const _r = nippleR + rm;
+
+    if (Math.pow(_x - _r + rm, 2) + Math.pow(_y - _r + rm, 2) < Math.pow(_r, 2)) {
+      return { x: _x, y: _y };
+    } else {
+      return false; 
+    }
+  }
+
+  nippleWrap.addEventListener('touchstart', function(e) {
+    nippleTouch = e.targetTouches[e.targetTouches.length - 1].identifier;
+  }, false);
+
+  btn.addEventListener('touchstart', function(e) {
+    btnTouch = e.targetTouches[e.targetTouches.length - 1];
+  }, false);
+
+  window.addEventListener('touchmove', function(e) {
+    for (let i = 0; i < e.changedTouches.length; i++) {
+      const touch = e.changedTouches[i];
+
+      switch(touch.identifier) {
+        case nippleTouch:
+          const intersection = checkIntersection(nippleWrap, touch.pageX, touch.pageY, -(nipple.offsetWidth / 2));
+          if (intersection) {
+            posNipple(intersection);
+          } else {
+            const _r = nippleR - nipple.offsetWidth / 2;
+            const x1 = nippleWrap.offsetLeft + _r;
+            const y1 = nippleWrap.offsetTop + _r;
+            const x2 = touch.pageX;
+            const y2 = touch.pageY;
+            const d = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+            posNipple({
+              x: ((x2 - x1) / d * _r) + nippleR,
+              y: ((y2 - y1) / d * _r) + nippleR
+            });
+          }
+          break;
+
+        case btnTouch:
+
+          break;
+
+      }
+    }
+  }, false);
+
+  window.addEventListener('touchend', function(e) {
+    // console.log(e);
+    console.log('end');
+  }, false);
+
+
+  //
+
+
+  // resize
+
+  function resize() {
+    nippleWrap.style.width = nippleWrap.offsetHeight + 'px';
+    nippleWrap.style.left = nippleWrap.offsetTop + 'px';
+    btn.style.width = btn.offsetHeight + 'px';
+    btn.style.right = nippleWrap.style.left;
+    nippleSide = nipple.offsetWidth;
+    nippleR = nippleWrap.offsetWidth / 2;
+    nipple.style.marginLeft = nipple.style.marginTop = (- nippleSide / 2) + 'px';
+    if (!nippleTouch) {
+      posNipple(coordsToPos({x: 0, y: 0}));
+    }
+  };
+
+  resize();
+
+  window.addEventListener('resize', resize);
+
+});
